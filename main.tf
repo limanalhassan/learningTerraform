eb1e40964a4637853e7c228c80d8033e78f106e8
limanalhassan <liman@linux.com>
2022-11-03 13:22:32 +0000

    Add vpc config

--- main.tf
+++ main.tf
@@ -14,9 +14,19 @@ data "aws_ami" "app_ami" {
   owners = ["979382823631"] # Bitnami
 }
 
+resource "aws_vpc" "my_vpc" {
+  cidr_block       = "10.0.0.0/16"
+  instance_tenancy = "default"
+
+  tags = {
+    Name = "main"
+  }
+}
+
 resource "aws_instance" "web" {
   ami           = data.aws_ami.app_ami.id
   instance_type = "t3.nano"
+  vpc_id        = aws_vpc.my_vpc.id
 
   tags = {
     Name = "HelloWorld"
